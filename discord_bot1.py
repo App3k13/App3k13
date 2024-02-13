287733fa1fb00a4f34643928d1c6db4f07fb7bd9
what name? <156357568+App3k13@users.noreply.github.com>
2024-02-13 19:02:00 +0100

    Update discord_bot1.py

--- discord_bot1.py
+++ discord_bot1.py
@@ -1,5 +1,7 @@
 import discord
 import pas_gen
+import os
+import random 
 
 from discord.ext import commands
 intents = discord.Intents.default()
@@ -11,21 +13,32 @@ bot = commands.Bot(command_prefix='/', intents=intents)
 async def on_ready():
     print(f'Zalogowaliśmy się jako {bot.user}')
 
-@bot.command()
-async def joined(ctx, member: discord.Member):
-    await ctx.send(f'{member.name} dołączył {discord.utils.format_dt(member.joined_at)}')
 
 @bot.command()
 async def hello(ctx):
-    await ctx.send(f'Cześć!')
+    """ Bot się z tobą wita """
+    await ctx.send(f'Hej!')
 
 @bot.command()
 async def bye(ctx):
-    await ctx.send(f'Do zobaczenia!')
+    """ Bot się z tobą żegna """
+    await ctx.send(f'Do widzenia!')
 
 @bot.command()
-async def goodmorning(ctx):
-    await ctx.send(f'Dzień dobry!')   
+async def meme(ctx):
+    """ Bot wysyła mema"""
+    all_images_in_folder = os.listdir('images')
+    random_image = random.choice(all_images_in_folder)
+    with open(f'images/{random_image}', 'rb') as f:
+        picture = discord.File(f)
+    await ctx.send(file=picture)
+
+advices = ["Rób zakupy z materiałową torbą.", "Zamień wodę butelkowaną na wodę z kranu.","Kupuj kosmetyki naturalne w opakowaniach nadających się do recyklingu.","Kupuj produkty na wagę.","Nie drukuj, jeśli nie musisz, albo drukuj dwustronnie.","Wybieraj rachunki oraz faktury w wersji elektronicznej."]
+@bot.command()
+async def environmentadvice(ctx):
+    """ Wybiera losową radę na temat środowiska """
+    advice = random.choice(advices)
+    await ctx.send(advice) 
 
 
 bot.run("Token is here")
